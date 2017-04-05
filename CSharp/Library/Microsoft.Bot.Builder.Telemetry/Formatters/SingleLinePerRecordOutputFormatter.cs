2dda346c5ac629cc452fc9dff1fd1dd085619c83
sbohlen <sbohlen@gmail.com>
2017-04-05 07:02:30 -0400

    initial add of DebugWindowTelemetryWriter and SingleLinePerRecordOutputFormatter

---
+++ CSharp/Library/Microsoft.Bot.Builder.Telemetry/Formatters/SingleLinePerRecordOutputFormatter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Bot.Builder.Internals.Fibers;
+
+namespace Microsoft.Bot.Builder.Telemetry.Formatters
+{
+    public class SingleLinePerRecordOutputFormatter : ITelemetryOutputFormatter
+    {
+        private ITelemetryContext _context;
+
+        public SingleLinePerRecordOutputFormatter(ITelemetryContext context)
+        {
+            SetField.NotNull(out _context, nameof(context), context);
+        }
+
+        private string GetDateTimeString()
+        {
+            return DateTimeOffset.Now.ToString("yyyy:MM:dd\thh:mm:ss.fff");
+        }
+        public string FormatCounter(string counter)
+        {
+            return $"{GetDateTimeString()}\t{GetBotContextProperties()}\tCounter: [{counter}]";
+        }
+
+        public string FormatEntity(string kind, string value)
+        {
+            return $"{GetDateTimeString()}\t{GetBotContextProperties()}\tEntity: [{kind}]-[{value}]";
+        }
+
+        public string FormatException(string component, string context, Exception e)
+        {
+            return $"{GetDateTimeString()}\t{GetBotContextProperties()}\tException: [{component} with [{context}]" + "\n" +
+                   $"{GetDateTimeString()}\t{GetBotContextProperties()}\t{e}";
+        }
+
+        public string FormatLogIntent(string intent, float score)
+        {
+            return $"{GetDateTimeString()}\t{GetBotContextProperties()}\tIntent: [{intent}] - {score}";
+        }
+
+        public string FormatServiceResult(string serviceName, DateTime startTime, DateTime endDateTime, string result, bool success = true)
+        {
+            return $"{GetDateTimeString()}\t{GetBotContextProperties()}\tServiceResult: [{serviceName}] - result: {result} - duration(ms): {endDateTime.Subtract(startTime).TotalMilliseconds} - success: {success}";
+        }
+
+        public void SetContext(ITelemetryContext context)
+        {
+            _context = context;
+        }
+
+        private string GetBotContextProperties()
+        {
+            return $"correlationId: {_context.CorrelationId}\tchannelId: {_context.ChannelId}\tconversationId: {_context.ConversationId}\tactivityId: {_context.ActivityId}\tuserId: {_context.UserId}\ttimestamp: {_context.Timestamp}";
+        }
+
+        public string FormatEvent(Dictionary<string, double> metrics)
+        {
+            var message = new StringBuilder();
+
+            foreach (var metric in metrics)
+            {
+                message.Append($"{{{metric.Key},{metric.Value}}}");
+            }
+
+            return $"{GetDateTimeString()}\t{GetBotContextProperties()}\tEvent: {message}";
+        }
+
+        public string FormatEvent(Dictionary<string, string> properties, Dictionary<string, double> metrics = null)
+        {
+            var message = new StringBuilder();
+
+            foreach (var property in properties)
+            {
+                message.Append($"{{{property.Key},{property.Value}}}");
+            }
+
+            if (null != metrics)
+            {
+                foreach (var metric in metrics)
+                {
+                    message.Append($"{{{metric.Key},{metric.Value}}}");
+                }
+            }
+
+            return $"{GetDateTimeString()}\t{GetBotContextProperties()}\tEvent: {message}";
+        }
+    }
+}
---
+++ CSharp/Library/Microsoft.Bot.Builder.Telemetry/Writers/DebugWindowTelemetryWriter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Internals.Fibers;
+
+namespace Microsoft.Bot.Builder.Telemetry.Writers
+{
+    public class DebugWindowTelemetryWriter : ITelemetryWriter
+    {
+        private bool _disposed;
+        private readonly ITelemetryOutputFormatter _outputFormatter;
+
+        public DebugWindowTelemetryWriter(ITelemetryOutputFormatter formatter)
+        {
+            SetField.NotNull(out _outputFormatter, nameof(formatter), formatter);
+        }
+
+        public async Task WriteCounterAsync(string counter)
+        {
+            Debug.WriteLine(_outputFormatter.FormatCounter(counter));
+        }
+
+        public async Task WriteExceptionAsync(string component, string context, Exception e)
+        {
+            Debug.WriteLine(_outputFormatter.FormatException(component, context, e));
+        }
+
+        public async Task WriteServiceResultAsync(string serviceName, DateTime startTime, DateTime endDateTime, string result, bool success = true)
+        {
+            Debug.WriteLine(_outputFormatter.FormatServiceResult(serviceName, startTime, endDateTime, result, success));
+        }
+
+        public async Task WriteEntityAsync(string kind, string value)
+        {
+            Debug.WriteLine(_outputFormatter.FormatEntity(kind, value));
+        }
+
+        public async Task WriteIntentAsync(string intent, float score, Dictionary<string, string> entities = null)
+        {
+            Debug.WriteLine(_outputFormatter.FormatLogIntent(intent, score));
+
+            if (null != entities)
+            {
+                foreach (var entity in entities)
+                {
+                    await WriteEntityAsync(entity.Key, entity.Value);
+                }
+            }
+        }
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                //
+            }
+
+            // Free any unmanaged objects here.
+            //
+            _disposed = true;
+        }
+
+        ~DebugWindowTelemetryWriter()
+        {
+            Dispose(false);
+        }
+
+        public void SetContext(ITelemetryContext context)
+        {
+            _outputFormatter.SetContext(context);
+        }
+
+        public async Task WriteEventAsync(string key, string value)
+        {
+            await WriteEventAsync(new Dictionary<string, string> { { key, value } });
+        }
+
+        public async Task WriteEventAsync(string key, double value)
+        {
+            await WriteEventAsync(new Dictionary<string, double> { { key, value } });
+        }
+
+        public async Task WriteEventAsync(Dictionary<string, double> metrics)
+        {
+            await WriteEventAsync(new Dictionary<string, string>(), metrics);
+        }
+
+        public async Task WriteEventAsync(Dictionary<string, string> properties, Dictionary<string, double> metrics = null)
+        {
+            Debug.WriteLine(_outputFormatter.FormatEvent(properties, metrics));
+        }
+    }
+}
