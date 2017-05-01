0b191efad74dee43dcb037b74f9098f6ff18cc69
sbohlen <sbohlen@gmail.com>
2017-05-01 19:17:44 -0400

    tests for TelemetryReporterRegistration

---
+++ CSharp/Tests/Microsoft.Bot.Builder.Telemetry.Tests/TelemetryModuleTests/TelemetryReporterRegistrationTests.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Telemetry.Tests.TelemetryModuleTests
+{
+    [TestClass]
+    public class TelemetryReporterRegistrationTests
+    {
+        private ContainerBuilder _containerBuilder;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _containerBuilder = new ContainerBuilder();
+        }
+
+        [TestMethod]
+        public void CanResolveTelemetryReporter()
+        {
+            _containerBuilder.RegisterModule(new TelemetryModule(new TelemetryModuleConfiguration()));
+            var container = _containerBuilder.Build();
+
+            Assert.IsTrue(container.IsRegistered<ITelemetryReporter>());
+        }
+    }
+}
