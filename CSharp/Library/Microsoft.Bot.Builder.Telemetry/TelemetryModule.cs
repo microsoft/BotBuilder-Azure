e77aae72f2de3beb406dac0ab9240625825b18fb
sbohlen <sbohlen@gmail.com>
2017-05-01 15:40:59 -0400

    initial rough structure of TelemetryModule

---
+++ CSharp/Library/Microsoft.Bot.Builder.Telemetry/TelemetryModule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Autofac;
+using Microsoft.Bot.Builder.Autofac.Base;
+using Microsoft.Bot.Builder.Internals.Fibers;
+
+namespace Microsoft.Bot.Builder.Telemetry
+{
+    public class TelemetryModule : Module
+    {
+        private readonly TelemetryModuleConfiguration _configuration;
+
+        public TelemetryModule(TelemetryModuleConfiguration configuration)
+        {
+            SetField.NotNull(out _configuration, nameof(configuration), configuration);
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            //ensure all provided Configuration instances are registered
+            foreach (var configuration in _configuration.TelemetryWriterConfigurations)
+            {
+                builder.RegisterInstance(configuration).AsSelf().SingleInstance();
+            }
+
+            base.Load(builder);
+        }
+
+    }
+
+    public class TelemetryModuleConfiguration
+    {
+        public TelemetryWriterDiscoveryStrategy WriterDiscoveryStrategy { get; set; }
+        public IList<object> TelemetryWriterConfigurations { get; set; }
+
+
+        public TelemetryModuleConfiguration()
+        {
+            TelemetryWriterConfigurations = new List<object>();
+        }
+    }
+
+    [Flags]
+    public enum TelemetryWriterDiscoveryStrategy
+    {
+        ExplicitFileSystemLocation = 1,
+        AssemblyFileSystemLocation = 2,
+        ExplicitlyDeclared = 4,
+        PreregisteredWithContainer = 8,
+    }
+}
---
+++ CSharp/Tests/Microsoft.Bot.Builder.Telemetry.Tests/TelemetryModuleTests/ValidateConfigurationBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Microsoft.Bot.Builder.Telemetry.DebugWriter;
+using Microsoft.Bot.Builder.Telemetry.TextFileWriter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Telemetry.Tests.TelemetryModuleTests
+{
+    [TestClass]
+    public class ValidateConfigurationBehavior
+    {
+        private ContainerBuilder _containerBuilder;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _containerBuilder = new ContainerBuilder();
+        }
+
+
+        [TestMethod]
+        public void CanRegisterSingleProvidedWriterConfiguration()
+        {
+            var config = new TelemetryModuleConfiguration();
+            config.TelemetryWriterConfigurations.Add(new TextFileTelemetryWriterConfiguration(new PerDayShardStrategy()));
+
+            _containerBuilder.RegisterModule(new TelemetryModule(config));
+            var container = _containerBuilder.Build();
+
+            Assert.IsTrue(container.IsRegistered<TextFileTelemetryWriterConfiguration>());
+        }
+
+        [TestMethod]
+        public void CanRegisterMultipleProvidedWriterConfigurations()
+        {
+            var config = new TelemetryModuleConfiguration();
+            config.TelemetryWriterConfigurations.Add(new TextFileTelemetryWriterConfiguration(new PerDayShardStrategy()));
+            config.TelemetryWriterConfigurations.Add(new DebugWindowTelemetryWriterConfiguration());
+
+            _containerBuilder.RegisterModule(new TelemetryModule(config));
+            var container = _containerBuilder.Build();
+
+            Assert.IsTrue(container.IsRegistered<TextFileTelemetryWriterConfiguration>());
+            Assert.IsTrue(container.IsRegistered<DebugWindowTelemetryWriterConfiguration>());
+        }
+    }
+}
