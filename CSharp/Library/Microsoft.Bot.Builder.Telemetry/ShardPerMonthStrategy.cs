4af33732b76892124f28b952e3dfec4fc8ec51d2
sbohlen <sbohlen@gmail.com>
2017-05-11 09:39:54 -0400

    introduce monthly sharding strategy

---
+++ CSharp/Library/Microsoft.Bot.Builder.Telemetry/ShardPerMonthStrategy.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Microsoft.Bot.Builder.Telemetry
+{
+    public class ShardPerMonthStrategy : IShardStrategy
+    {
+        public string CurrentShardKey => $"{DateTime.UtcNow:yyyy-MM}";
+    }
+}
