d88b6dafa36e5b7426d5eda9140d379f181b57be
sbohlen <sbohlen@gmail.com>
2017-08-18 11:07:26 -0400

    build and packaging scripts

---
+++ CSharp/Library/Microsoft.Bot.Builder.Extensions.Azure.Telemetry.BlobStorageWriter/createpackage.cmd
@@ -0,0 +1,14 @@
+﻿@echo off
+echo *** Building Microsoft.Bot.Builder.Extensions.Azure.Telemetry.BlobStorageWriter
+setlocal
+setlocal enabledelayedexpansion
+setlocal enableextensions
+set errorlevel=0
+mkdir ..\nuget
+erase /s ..\nuget\Microsoft.Bot.Builder.Extensions.Azure.Telemetry.BlobStorageWriter*nupkg
+msbuild /property:Configuration=release Microsoft.Bot.Builder.Extensions.Azure.Telemetry.BlobStorageWriter.csproj 
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.Extensions.Telemetry.dll).FileVersionInfo.FileVersion"') do set telemetry=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.Extensions.Azure.Telemetry.BlobStorageWriter.dll).FileVersionInfo.FileVersion"') do set version=%%v
+..\..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.Extensions.Azure.Telemetry.BlobStorageWriter.nuspec -symbols -properties version=%version%;telemetry=%telemetry% -OutputDirectory ..\nuget
+echo *** Finished building Microsoft.Bot.Builder.Extensions.Azure.Telemetry.BlobStorageWriter
+
